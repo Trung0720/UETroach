cf164b3a91f113d1b5aaeb5832b372972938efba
Trung0720 <24022474@vnu.edu.vn>
2025-04-25 01:08:40 +0700

    add menu resources

---
+++ src/uet/oop/bomberman/Main.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import uet.oop.bomberman.graphics.Sprite;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch(Main.class);
+    }
+}
