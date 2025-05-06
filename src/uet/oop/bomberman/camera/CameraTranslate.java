63033bf2086d354838a6661c994f347386a93b05
Trung0720 <24022474@vnu.edu.vn>
2025-05-06 08:05:15 +0700

    update camera translate

--- src/uet/oop/bomberman/camera/CameraTranslate.java
+++ src/uet/oop/bomberman/camera/CameraTranslate.java
@@ -4,10 +4,6 @@ import uet.oop.bomberman.Main;
 
 public class CameraTranslate {
     public static void moveCamera(int x, int y) {
-        Main.graphicsContext.clearRect(0, 0,
-                Main.canvas.getWidth(),
-                Main.canvas.getHeight()
-        );
         Main.graphicsContext.translate(-x, -y);
         Main.cameraX -= x;
         Main.cameraY -= y;
